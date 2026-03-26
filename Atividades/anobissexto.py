e66247c4616095c048cf09550153b0a8db86e885
Eduardo Fontes <134387039+FontesEdu@users.noreply.github.com>
2026-03-26 01:58:13 +0000

    correção

--- Atividades/anobissexto.py
+++ Atividades/anobissexto.py
@@ -1,11 +1,9 @@
-# ano = int(input("Informe um ano: "))
+ano = int(input("Informe um ano: "))
 
-# if (ano % 4 == 0 and ano % 100 != 0) or (ano % 400 == 0):
-#     print(f"{ano} é um ano bissexto.")
-# else:
-#     print(f"{ano} não é um ano bissexto.")
-
-def verificar_ano_bissexto(year):
-    return (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0)
-
-print(verificar_ano_bissexto(2000))
+if ano > 0: 
+    if(ano % 4 == 0 and ano % 100 != 0) or (ano % 400 == 0):
+        print(f"{ano} é um ano bissexto.")
+    else:
+        print(f"{ano} não é um ano bissexto.")
+else: 
+    print("O ano deve ser maior que zero.")
